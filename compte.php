9f3c7dd32ebf7a00b0e00de40115ad54a737f49e
Unknown <Clement.Orlandini.Etu@univ-lemans.fr>
2018-04-25 16:11:50 +0200

    Prototype final légères modifs

---
+++ compte.php
@@ -0,0 +1,23 @@
+<?php
+
+header("Content-type: text/html; charset=UTF-8");
+
+/*require("param.inc.php");
+$pdo = new PDO("mysql:host=".MYHOST.";dbname=".MYDB, MYUSER, MYPASS);
+$pdo->query("SET NAMES utf8");
+$pdo->query("SET CHARACTER SET 'utf-8'");
+
+if(isset($_POST['conform']))
+{
+    if(!empty($_POST['pseudo']) AND !empty($_POST['email']) AND !empty($_POST['confirmeremail']) AND !empty($_POST['sexe']) AND !empty($_POST['mdp']) AND !empty($_POST['confirmermdp']) AND !empty($_POST['confirmermdp']))
+    {
+     echo ("ok");   
+    }else{
+     echo ("non");
+}
+}*/
+
+
+echo("test");
+      
+?>    
