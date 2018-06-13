6fa1eb87d33ebb83db2e3854d2b773c809d8df48
Unknown <hocquellet.sarah@gmail.com>
2018-06-13 17:10:37 +0200

    mis à jour des headers

---
+++ header.php
@@ -0,0 +1,111 @@
+<?php
+
+session_start();
+
+
+
+if (!empty($_SESSION['idJoueur'])){
+
+?>
+
+  <header>
+        
+        <div>
+        
+        <img id="iconeMenu" src="medias/images/deroulant.png" alt="Menu déroulant" />
+        <nav> 
+            <ul class="menuDeroulant" >
+                <li><a href="seConnecter.php">Se deconnecter</a></li>
+
+                <li><a href="index.php">Accueil</a></li>
+
+                <li><a href="jeu.php">Jouer en solo</a></li>
+            </ul>
+        </nav>
+        
+        <div class="reseaux">
+            
+            <a href="https://twitter.com/Ongaku76824917"> <img class="twitter" src="medias/images/twitter.png" alt="twitter" /></a>
+            
+            <a href="https://www.facebook.com/OngakuMMi/?modal=admin_todo_tour"> <img class="facebook" src="medias/images/facebook.png" alt="facebook" /></a>
+            
+        </div>
+        </div>
+
+        <h1><a href="index.php" >ONGAKU</a></h1>
+
+        <div>
+           <!-- <a href="chat.html"> <img src="medias/images/logoChat.png" alt="Icône du chat" /></a> -->
+             <img id="iconeRegle" src="medias/images/boutonLivreFerme.png" alt="Règles">
+                <section id="nav2">
+                    <ul class="menuDeroulant">
+                        <li>Vous pouvez remplir les mots de la musique pendant 45 secondes (30 secondes de musique et 15 secondes supplémentaires pour écrire)
+                        <li>Le seul moyen pour compléter les mots manquant c'est de les écrire dans la barre de réponse</li>
+                        <li>Vous pouvez mettre tous les mots en même temps dans la barre de réponse même s'ils ne se suivent pas dans les paroles</li>
+                        <li>Une partie est composée de 10 chansons différentes</li>
+                    </ul>
+                </section>
+
+            <a href="profil.php"> <img class="iconeMembre" src="medias/images/logoFeminin.png" alt="Icône du profil" /></a>
+        </div>
+
+    </header>
+
+
+
+<?php
+} else {
+?>
+
+<header>
+            
+            <div>
+            
+               <img id="iconeMenu" src="medias/images/deroulant.png" alt="Menu déroulant"/>
+                <nav>
+                        <ul class="menuDeroulant">
+                            <li><a href="CreerUnCompte.php">creer un compte</a></li>
+                            
+                            <li><a href="index.php">Accueil</a></li>
+                            
+                            <li><a href="jeu.php">Jouer en solo</a></li>
+                        </ul>
+              </nav>
+            <div class="reseaux">
+            
+            <a href="https://twitter.com/Ongaku76824917"> <img class="twitter" src="medias/images/twitter.png" alt="twitter" /></a>
+            
+            <a href="https://www.facebook.com/OngakuMMi/?modal=admin_todo_tour"> <img class="facebook" src="medias/images/facebook.png" alt="facebook" /></a>
+            
+        </div>
+                </div>
+            
+                  <h1><a href="index.php" >ONGAKU</a></h1>
+
+            <div>
+                <img id="iconeRegle" src="medias/images/boutonLivreFerme.png" alt="Règles">
+                <section id="nav2">
+                    <ul class="menuDeroulant">
+                        <li>Vous pouvez remplir les mots de la musique pendant 45 secondes (30 secondes de musique et 15 secondes supplémentaires pour écrire)
+                        <li>Le seul moyen pour compléter les mots manquant c'est de les écrire dans la barre de réponse</li>
+                        <li>Vous pouvez mettre tous les mots en même temps dans la barre de réponse même s'ils ne se suivent pas dans les paroles</li>
+                        <li>Une partie est composée de 10 chansons différentes</li>
+                    </ul>
+                </section>
+                <a href="seConnecter.php" > <img class="iconeMembre" src="medias/images/logoInvite.png" alt ="Icône du profil"/></a>
+            </div>
+    
+        </header>
+
+
+
+
+<?php
+}
+?>
+
+
+
+
+
+
--- js/header.js
+++ js/header.js
@@ -4,15 +4,18 @@ document.addEventListener("DOMContentLoaded",initialiser);
 function initialiser(evt){
      var menuDeroulant = document.getElementById("iconeMenu");
      menuDeroulant.addEventListener("click", cliquer); 
+    
+var regles = document.getElementById("iconeRegle");
+     regles.addEventListener("click", cliquer2);
 
 }   
     
     
 function cliquer(evt){
-    document.querySelector(".menuDeroulant").classList.toggle("apparaitre") ;
+    document.querySelector(".menuDeroulant").classList.toggle("apparaitre");
     
     var image = document.getElementById("iconeMenu");
-    console.log(image.getAttribute("src"));
+//    console.log(image.getAttribute("src"));
     
     if(image.getAttribute("src") == "medias/images/deroulant.png"){
         image.src = "medias/images/croixMenuDeroulant.png";
@@ -20,5 +23,21 @@ function cliquer(evt){
         
     }else{
          image.src = "medias/images/deroulant.png";
+    }
+}
+
+
+
+function cliquer2(evt){
+    document.querySelectorAll(".menuDeroulant")[1].classList.toggle("apparaitre") ;
+    
+    var image = document.getElementById("iconeRegle");
+    
+    if(image.getAttribute("src") == "medias/images/boutonLivreFerme.png"){
+        image.src = "medias/images/boutonLivreOuvert.png";
+        
+        
+    }else{
+         image.src = "medias/images/boutonLivreFerme.png";
     }
 }
