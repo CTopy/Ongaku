7a26558f1dbc19694c197ec40a5076f7857cef08
Unknown <lejean.florian@gmail.com>
2018-06-14 10:06:05 +0200

    suppression param.inc.php

--- param.inc.php
+++
@@ -1,9 +0,0 @@
-<?php 
-if(!defined("MYHOST"))
-    define("MYHOST", "localhost");
-if(!defined("MYUSER"))
-    define("MYUSER", "17mmi1pj10");
-if(!defined("MYPASS"))
-    define("MYPASS", "PRPXgp0s");
-if(!defined("MYDB"))
-    define("MYDB", "17mmi1pj10");
